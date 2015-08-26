50a15b60da9bdd46901ee4dc5b36c505218b0468
Biletnikoff <bogere.sanders@gmail.com>
2015-08-26 17:08:21 -0400

    redone with when

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,22 @@
+
+#pushing lang array into a new array using yield
 def my_collect(lang)
-    i = 0
-    array = []
-  until i == lang.length
+  i = 0
+#collect method returns a new array this array allows me to push to new array
+  collect = [] 
 
-    array << yield(lang[i])
-
+  while i < lang.length
+    #pushes each index in the array to new array 
+    collect << yield(lang[i])
+    #this allows us to select each item in the index one at a time.
     i += 1
-      
   end
-lang 
-array
+collect
 end
+
+
+    
+
 
 
 
