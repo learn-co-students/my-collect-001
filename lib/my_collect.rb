82f1f3b3b7e3b285baa4f09deed188602fd0b3dc
thaik1618 <kthai1618@gmail.com>
2016-02-23 16:18:33 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,10 @@
 def my_collect(array)
   i = 0
+  new_array = []
   while i < array.length
-    yield array[i]
+    new_array << yield(array[i])
     i += 1
   end
-  array.collect do |item|
-    item.upcase
-  end
+  new_array
 end
 
