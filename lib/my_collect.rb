0a554428ad014ef2ea5c243f6429d7fb8fb09f67
Dima Rogov <navydimka@gmail.com>
2016-06-05 19:25:34 +0300

    have done my own collect function

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(collection)
+  amount = 0
+  new_collection = []
+  while amount < collection.length
+    element = collection[amount]
+    new_collection << yield(element)
+    amount += 1
+  end
+  new_collection
+end
