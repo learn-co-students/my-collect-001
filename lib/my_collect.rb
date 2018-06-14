2c5876ccfe3c15ef398b2bf2a624406e6885ce7f
Tony Turetsky <TTuretsky@gmail.com>
2018-06-14 12:41:31 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+  counter = 0
+  result = []
 
+  while counter < array.size
+    result << yield(array[counter])
+    counter += 1
+  end
+  result
+end
 
+# if block, then yield to block # also need to pass argument(s) to yield, otherwise block is operating on nil
